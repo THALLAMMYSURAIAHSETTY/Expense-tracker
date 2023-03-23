54b6b69bc8132e28d6ab9d3dc7baa3d9e8505a8e
THALLAMMYSURAIAHSETTY <119222831+THALLAMMYSURAIAHSETTY@users.noreply.github.com>
2023-03-23 07:29:54 +0530

    1 file added

---
+++ src/Year2021.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+export default function Year2021() {
+  return (
+    <div>
+      <h1>2021</h1>
+    </div>
+  );
+}
