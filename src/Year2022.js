12f119532e25a0f048ce622ada426ac38d0711f3
THALLAMMYSURAIAHSETTY <119222831+THALLAMMYSURAIAHSETTY@users.noreply.github.com>
2023-03-23 07:30:49 +0530

    1 file added

---
+++ src/Year2022.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+export default function Year2022() {
+  return (
+    <div>
+      <h1>2022</h1>
+    </div>
+  );
+}
