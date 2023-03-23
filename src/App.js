63498df676b555533598a24ea10ee1eea18f75e8
THALLAMMYSURAIAHSETTY <119222831+THALLAMMYSURAIAHSETTY@users.noreply.github.com>
2023-03-23 07:28:49 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./style.css";
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Year2023 from "./Year2023"
+import Year2022 from "./Year2022"
+import Year2021 from "./Year2021"
+
+
 
 export default function App() {
+
+  let[Year , setYear]=useState(2023)
+
+  function Changeyear()
+  {
+    let Yeardropdown = document.getElementById('Yeardropdown').value
+    setYear(Yeardropdown)
+    console.log(Yeardropdown)
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Expense Tracker</h1>
+      <select name="Year" id="Yeardropdown" onChange={Changeyear}>
+        <option value="2023" selected="selected">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+      </select>
+      {/* <Routes>
+        <Route path="/" element={<Year2023/>}/>
+      </Routes> */}
+      {Year=="2023" ? <Year2023/> : Year=="2022" ? <Year2022/> : <Year2021/>}
     </div>
   );
 }
